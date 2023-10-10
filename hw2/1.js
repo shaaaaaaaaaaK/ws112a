a30fb09f1a7a005b5574914babdd7e91b8a815b3
Shaaaaaaaaark <bella921127@gmail.com>
2023-10-10 13:20:57 +0800

    re

--- hw2/1.js
+++ hw2/1.js
@@ -32,24 +32,40 @@ router
       let pass = params['pass']
       console.log(`name=${name} pass=${pass}`)
       if (peoples.get(name)) {
-        ctx.response.body = {'error':`name=${name} 已經存在！`}
+        ctx.response.body = '此帳號已存在'
       } else {
         peoples.set(name, {name, pass})
         ctx.response.type = 'text/html'
-        ctx.response.body = `<p>新增 (${name}, ${pass}) 成功</p><p><a href="/people/">列出所有人員</a></p>`
+        ctx.response.body = `<p>註冊成功</p><p><a href="http://127.0.0.1:8000/public/find.html">登入</a></p>`
       }
   
     }
 
   })
-  .get("/people/find", (ctx) => {
-    let params = ctx.request.url.searchParams    
-    let name = params.get('name')
-    console.log('name=', name)
-    if (peoples.has(name)) {
-      ctx.response.body = peoples.get(name);
-    }
-  })
+  .post("/people/find",async (ctx) => {
+    const body = ctx.request.body()
+    if (body.type === "form") {
+      const pairs = await body.value
+      console.log('pairs=', pairs)
+      const params = {}
+      for (const [key, value] of pairs) {
+        params[key] = value
+      }
+      console.log('params=', params)
+      let name = params['name']
+      let pass = params['pass']
+      console.log(`name=${name} pass=${pass}`)
+      if (peoples.get(name) && pass==peoples.get(name).pass) {
+        ctx.response.type = 'text/html'
+        ctx.response.body = '登入成功'
+      } 
+      else {
+        ctx.response.type = 'text/html'
+        ctx.response.body = `<p>登入失敗</p><p><a href="http://127.0.0.1:8000/public/find.html">登入</a></p>`
+      }
+      console.log("key:",peoples.get(name).pass)
+  }
+})
   .get("/public/(.*)", async (ctx) => {
     let wpath = ctx.params[0]
     console.log('wpath=', wpath)
