e2cc98b9ad9987a3fd8b9b280b2bb5c55563d0e8
Shaaaaaaaaark <bella921127@gmail.com>
2023-10-20 09:38:27 +0800

    re

---
+++ HW3/render.js
@@ -0,0 +1,105 @@
+export function layout(title, content) {
+    return `
+    <html>
+    <head>
+      <title>${title}</title>
+      <style>
+        body {
+          padding: 80px;
+          font: 16px Helvetica, Arial;
+        }
+    
+        h1 {
+          font-size: 2em;
+        }
+    
+        h2 {
+          font-size: 1.2em;
+        }
+    
+        #posts {
+          margin: 0;
+          padding: 0;
+        }
+    
+        #posts li {
+          margin: 40px 0;
+          padding: 0;
+          padding-bottom: 20px;
+          border-bottom: 1px solid #eee;
+          list-style: none;
+        }
+    
+        #posts li:last-child {
+          border-bottom: none;
+        }
+    
+        textarea {
+          width: 500px;
+          height: 300px;
+        }
+    
+        input[type=text],
+        textarea {
+          border: 1px solid #eee;
+          border-top-color: #ddd;
+          border-left-color: #ddd;
+          border-radius: 2px;
+          padding: 15px;
+          font-size: .8em;
+        }
+    
+        input[type=text] {
+          width: 500px;
+        }
+      </style>
+    </head>
+    <body>
+      <section id="content">
+        ${content}
+      </section>
+    </body>
+    </html>
+    `
+  }
+  
+  export function list(posts) {
+    let list = []
+    for (let post of posts) {
+      list.push(`
+      <li>
+        <h2>${ post.title }</h2>
+        <p><a href="/post/${post.id}">查看聯絡人</a></p>
+      </li>
+      `)
+    }
+    let content = `
+    <h1>Posts</h1>
+    <p>你有 <strong>${posts.length}</strong>個聯絡人~</p>
+    <p><a href="/post/new">增加新的聯絡人</a></p>
+    <ul id="posts">
+      ${list.join('\n')}
+    </ul>
+    `
+    return layout('Posts', content)
+  }
+  
+  export function newPost() {
+    return layout('New Post', `
+    <h1>新聯絡人</h1>
+    <p>建立新聯絡人</p>
+    <form action="/post" method="post">
+      <p><input type="text" placeholder="姓名" name="title"></p>
+      <p><textarea placeholder="電話" name="body"></textarea></p>
+      <p><input type="submit" value="新增"></p>
+    </form>
+    `)
+  }
+  
+  export function show(post) {
+    return layout(post.title, `
+      <h1>${post.title}</h1>
+      <p>${post.body}</p>
+    `)
+  }
+  
